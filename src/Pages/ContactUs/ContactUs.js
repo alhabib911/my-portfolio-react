aa9db8dec681f6abaa4e42afe09a9c87b1c0e935
alhabib911 <abdullahalhabib100@gmail.com>
2022-06-10 00:06:11 +0600

    contact form updated

--- src/Pages/ContactUs/ContactUs.js
+++ src/Pages/ContactUs/ContactUs.js
@@ -7,6 +7,8 @@ import { AiFillFacebook } from 'react-icons/ai';
 import { BsGithub } from 'react-icons/bs';
 import './ContactUs.css'
 import emailjs from '@emailjs/browser';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactUs = () => {
     const form = useRef();
@@ -14,11 +16,18 @@ const ContactUs = () => {
         e.preventDefault();
 
         emailjs.sendForm('service_9sa5s6g', 'template_oebzn9h', form.current, 'gRQt21wdg0IrHYKQS')
+
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
-            });
+            })
+            .then(() => {
+                toast('Email Sent')
+                e.target.reset()
+
+            })
+            ;
     };
     return (
         <div className='contact-container'>
@@ -68,16 +77,19 @@ const ContactUs = () => {
                         </div>
                     </div>
                 </div>
-                <form ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input type="text" name="user_name" />
-                    <label>Email</label>
-                    <input type="email" name="user_email" />
-                    <label>Message</label>
-                    <textarea name="message" />
-                    <input type="submit" value="Send" />
+                <form className='contact-form' ref={form} onSubmit={sendEmail}>
+                    <label>Name</label> <br />
+                    <input className='name-email' type="text" name="user_name" /> <br />
+                    <label>Email</label> <br />
+                    <input className='name-email' type="email" name="user_email" /> <br />
+                    <label>Message</label><br />
+                    <textarea name="message" /><br /> <br />
+                    <div className='message-submit-button'>
+                        <input type="submit" value="Send" />
+                    </div>
                 </form>
             </div>
+            <ToastContainer />
         </div>
     );
 };
