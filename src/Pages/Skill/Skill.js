b9c722e9caa80dba1b117941077199d37ba7b207
alhabib911 <abdullahalhabib100@gmail.com>
2022-06-09 10:38:49 +0600

    my skill added

--- src/Pages/Skill/Skill.js
+++ src/Pages/Skill/Skill.js
@@ -3,53 +3,141 @@ import './Skill.css'
 
 const Skill = () => {
     return (
-        <div className='skill-container'>
-            <div className="skill-title">
-                <div classNameName="skill-title">
-                    <h2>My Skill</h2>
-                </div>
-                <div classNameName="skill-details">
-                    <h2>HTML <small>90%</small></h2>
-                    <div className="progress-bar-details">
-                        <div className="skill-section">
-                            <div className="skill-name">
-                                <h2>HTML <small>75%</small></h2>
-                            </div>
-                            <div class="container">
-                                <div class="progress progress-striped">
-                                    <div class="progress-bar">
+        <div className="skill">
+            <div classNameName="skill-title">
+                <h6>My Skill</h6>
+            </div>
+            <div className='skill-container'>
+                    <div classNameName="skill-details">
+                        <div className="progress-bar-details">
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>React <small>80%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-1">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="skill-section">
-                            <div className="skill-name">
-                                <h2>HTML <small>75%</small></h2>
-                            </div>
-                            <div class="container">
-                                <div class="progress progress-striped">
-                                    <div class="progress-bar">
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>JavaScript <small>80%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-2">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="skill-section">
-                            <div className="skill-name">
-                                <h2>HTML <small>75%</small></h2>
-                            </div>
-                            <div class="container">
-                                <div class="progress progress-striped">
-                                    <div class="progress-bar">
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Node JS <small>75%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-3">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>MongoDB <small>75%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-4">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>HTML-5 <small>95%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-5">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>CSS3 <small>90%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-6">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Bootstrap <small>80%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-7">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Tailwind <small>85%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-8">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Daisy UI <small>90%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-11">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Express JS <small>75%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-9">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="skill-section">
+                                <div className="skill-name">
+                                    <h2>Firebase <small>70%</small></h2>
+                                </div>
+                                <div class="container">
+                                    <div class="progress progress-striped">
+                                        <div class="progress-bar-10">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
-                        
-                        
                     </div>
                 </div>
             </div>
-        </div>
+        // </div>
     );
 };
 
